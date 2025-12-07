60eab1ab91a9dcb3d530107768bf17e2c425781d
firoz.nur <firoznur5@gmail.com>
2025-12-07 09:45:55 +0600

    Add : Booking Constant

---
+++ src/app/modules/booking/booking.constant.ts
@@ -0,0 +1,10 @@
+export const bookingSearchableFields = ["pax", "status", "paymentStatus"];
+
+export const bookingFilterableFields = [
+  "member",
+  "package",
+  "status",
+  "paymentStatus",
+  "startDate",
+  "endDate",
+];
